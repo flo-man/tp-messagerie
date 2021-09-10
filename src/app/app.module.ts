448ef255c1219109896b55f0ab4f3bd39dc43044
Flo <flo.brown@me.com>
2021-09-10 17:08:25 +0200

    TP - Partie 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EnTeteComponent } from './en-tete/en-tete.component';
+import { ListePersonneComponent } from './liste-personne/liste-personne.component';
+import { ListeMessageComponent } from './liste-message/liste-message.component';
+import { FormMessageComponent } from './form-message/form-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnTeteComponent,
+    ListePersonneComponent,
+    ListeMessageComponent,
+    FormMessageComponent
   ],
   imports: [
     BrowserModule
